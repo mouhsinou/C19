37b51b7432d066c4be20529904619239e26fb845
Mouhsinou <mamamouhsinou@gmail.com>
2024-08-14 16:00:58 +0100

    first commit

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ for i in range(1, 65):
 
 # Button for making the prediction
 if st.button('Predict'):
-    response = requests.post('http://localhost:8000/predict/', json=input_data)
+    response = requests.post('https://covid-19-api3.onrender.com/predict/', json=input_data)
     if response.status_code == 200:
-        st.write(f'The predicted class is: {response.json()["prediction"]}')
+        st.write(f'RESULTAT DU DIAGNOSTIQUE : {response.json()["prediction"]}')
     else:
         st.write('Error in prediction. Please check the input values.')
 
-st.header('Upload CSV File for Batch Prediction')
+st.header('FAIRE LE DIAGNOSTIQUE DE PLUSIEUR PATIENT ')
 
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
@@ -29,9 +29,16 @@ if uploaded_file is not None:
         data = data.drop(columns=['Patient_ID'])
         
     if st.button('Predict for CSV'):
-        response = requests.post('http://localhost:8000/predict_batch/', json=data.to_dict(orient='records'))
+        response = requests.post('https://covid-19-api3.onrender.com/predict_batch/', json=data.to_dict(orient='records'))
         if response.status_code == 200:
             st.write('Predictions:')
             st.write(response.json()['predictions'])
         else:
             st.write('Error in prediction. Please check the input values.')
+
+with st.sidebar:
+    st.write("**Mes Coordonnées :**")
+    st.write("**Nom:** MAMA Moussinou")
+    st.write("**Email:** mamamouhsinou@gmail.com")
+    st.write("**Téléphone:** +229 95231680")
+    st.write("**LinkedIn:** [moussinou-mama-8b6270284](https://www.linkedin.com/in/moussinou-mama-8b6270284/)")
